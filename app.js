af2488998c8d2d7d8fbe9e433dd9c45687a48a58
Uma Sai Ramaraju <umaramaraju10@gmail.com>
2026-06-17 13:31:30 +0530

    Move MongoDB URI to env and remove node_modules

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/users");
@@ -6,9 +7,7 @@ const app = express();
 
 //!connect to mongodbc
 mongoose
-  .connect(
-    "mongodb://umaramaraju10_db_user:EGwr2fKB4mbWOutz@ac-v5xoukd-shard-00-00.hfjcevj.mongodb.net:27017,ac-v5xoukd-shard-00-01.hfjcevj.mongodb.net:27017,ac-v5xoukd-shard-00-02.hfjcevj.mongodb.net:27017/AuthenticationAPI?ssl=true&replicaSet=atlas-qeshkc-shard-0&authSource=admin&appName=Cluster0",
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Db connected successfully"))
   .catch((e) => console.log(e));
 
